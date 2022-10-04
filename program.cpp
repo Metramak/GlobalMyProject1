b5d26987a59ad11da50f553f09b313441262c936
Metramak <boomox.ff@gmail.com>
2022-10-04 18:13:49 +0300

    Snapshot1

---
+++ program.cpp
@@ -0,0 +1,12 @@
+//файл program.cpp 
+#include "stdafx.h" 
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{ 
+    cout << MySum(10, 3) << endl; 
+    cout << MySub(10, 3) << endl;
+    return 0; 
+}
